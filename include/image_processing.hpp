da24832c87e4c99bb3b1ce9aebd2f7da6532492e
valentina-kustikova <valentina.kustikova@gmail.com>
2016-06-28 08:59:35 +0300

    Abstract class for imgprocessing

---
+++ include/image_processing.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "opencv2/core/core.hpp"
+
+class ImageProcessor {
+ public:
+   virtual cv::Mat CvtColor(const cv::Mat &src, const cv::Rect &roi) = 0;
+   virtual cv::Mat Filter(const cv::Mat &src, const cv::Rect &roi, 
+     const int kSize) = 0;
+   virtual cv::Mat DetectEdges(const cv::Mat &src, const cv::Rect &roi, 
+     const int filterSize, const int lowThreshold, const int ratio, 
+     const int kernelSize) = 0;
+   virtual cv::Mat Pixelize(const cv::Mat &src, const cv::Rect &roi, 
+     const int kDivs) = 0;
+};
