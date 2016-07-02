89162546ca28dbbf6583ecea483f146c00772224
valentina-kustikova <valentina.kustikova@gmail.com>
2016-07-02 09:10:24 +0300

    Practice 1, template

---
+++ include/workaround.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "opencv2/core/core.hpp"
+
+class MatrixProcessor {
+ public:
+   virtual void Threshold(uchar* const data, const int width, 
+                          const int height, const int threshold);
+};
---
+++ samples/devtools_demo.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+
+#include "workaround.hpp"
+
+using namespace std;
+using namespace cv;
+
+const char* kAbout =
+    "This is an empty application that can be treated as a template for your "
+    "own doing-something-cool applications.";
+
+const char* kOptions =
+    "{ @image         |        | image to process         }"
+    "{ h ? help usage |        | print help message       }";
+
+
+int main(int argc, const char** argv) {
+  // Parse command line arguments.
+  CommandLineParser parser(argc, argv, kOptions);
+  parser.about(kAbout);
+
+  // If help option is given, print help message and exit.
+  if (parser.has("help")) {
+    parser.printMessage();
+    return 0;
+  }
+
+  // Read image.
+  Mat src = imread(parser.get<string>(0), CV_LOAD_IMAGE_GRAYSCALE);
+  if (src.empty()) {
+    cout << "Failed to open image file '" + parser.get<string>(0) + "'." 
+      << endl;
+    return 0;
+  }
+
+  // Show source image.
+  const string kSrcWindowName = "Source image";
+  const int kWaitKeyDelay = 1;  
+  namedWindow(kSrcWindowName);
+  imshow(kSrcWindowName, src);
+  waitKey(kWaitKeyDelay);
+
+  // Threshold data.
+  MatrixProcessor processor;
+  const int threshold = 128;
+  try {
+    processor.Threshold(src.data, src.cols, src.rows, threshold);
+  }
+  catch (const std::exception& ex) {
+    cout << ex.what() << endl;
+    return 0;
+  }  
+
+  // Show destination image.
+  const string kDstWindowName = "Destination image";
+  namedWindow(kDstWindowName);
+  imshow(kDstWindowName, src);
+  waitKey();
+
+  return 0;
+}
