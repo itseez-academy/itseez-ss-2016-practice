1dc0360371c848d76e3d7b42ba8f47d8b9ee7911
Pavel Druzhkov <druzhkov.paul@gmail.com>
2016-07-04 02:49:39 +0300

    make factory methods static

--- include/detection.hpp
+++ include/detection.hpp
@@ -7,7 +7,7 @@
 
 class Detector {
  public:
-  std::shared_ptr<Detector> CreateDetector(const std::string& name);
+  static std::shared_ptr<Detector> CreateDetector(const std::string& name);
   virtual bool Init(const std::string& model_file_path) = 0;
   virtual void Detect(const cv::Mat& frame, std::vector<cv::Rect>& objects,
                       std::vector<double>& scores) = 0;
