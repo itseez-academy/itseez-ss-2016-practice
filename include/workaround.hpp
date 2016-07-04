5785d1e2cae75267d9af2dd38cae2cff588d20ea
Школа Itseez2016 <iss2016@ccam.cmc.unn.net>
2016-07-04 16:49:39 +0300

    Full Based Task + 1 Extended task

--- include/workaround.hpp
+++ include/workaround.hpp
@@ -4,4 +4,11 @@ class MatrixProcessor {
  public:
   void Threshold(unsigned char* const data, const int width, const int height,
                  const int threshold);
+  void Medium(unsigned char* const data, const int width, const int height);
+  void Mediana(unsigned char* const data, const int width, const int height);
+private:
+	unsigned char MasToPixel(unsigned char* const data, int x, int y, int w, int h, int empty = 0);
+	unsigned char MasToPixelS(int x, int y, int empty = 0);
+	unsigned char * Mas;
+	int W, H;
 };
--- src/workaround.cpp
+++ src/workaround.cpp
@@ -6,5 +6,53 @@ using namespace std;
 
 void MatrixProcessor::Threshold(unsigned char* const data, const int width,
                                 const int height, const int threshold) {
-  // TODO: Add thresholding logic here.
+	for (int i = 0; i < width*height; i++)
+		if (data[i] < threshold)
+			data[i] = 0;
+}
+
+void MatrixProcessor::Medium(unsigned char* const data, const int width, const int height)
+{
+	Mas = data;
+	W = width;
+	H = height;
+	int i = 0;
+	for (int y = 0; y < width; y++)
+		for (int x = 0; x < height; y++)
+		{
+			data[i] = (MasToPixelS(x - 1, y - 1, data[i]) + MasToPixelS(x - 1, y, data[i]) + MasToPixelS(x - 1, y, data[i]) + MasToPixelS(x, y - 1, data[i]) +
+				MasToPixelS(x, y, data[i]) + MasToPixelS(x, y, data[i]) + MasToPixelS(x + 1, y - 1, data[i]) + MasToPixelS(x + 1, y, data[i]) +
+				MasToPixelS(x + 1, y, data[i])) / 9;
+			i++;
+		}
+}
+
+void MatrixProcessor::Mediana(unsigned char* const data, const int width, const int height)
+{
+	Mas = data;
+	W = width;
+	H = height;
+	int i = 0;
+	for (int y = 0; y < width; y++)
+		for (int x = 0; x < height; y++)
+		{
+			data[i] = (MasToPixelS(x - 1, y - 1, data[i]) + MasToPixelS(x - 1, y, data[i]) + MasToPixelS(x - 1, y + 1, data[i])
+				+ MasToPixelS(x, y - 1, data[i]) +	MasToPixelS(x, y, data[i]) + MasToPixelS(x, y + 1, data[i]) 
+				+ MasToPixelS(x + 1, y - 1, data[i]) + MasToPixelS(x + 1, y, data[i]) +	MasToPixelS(x + 1, y + 1, data[i])) / 9;
+			i++;
+		}
+}
+
+unsigned char MatrixProcessor::MasToPixelS(int x, int y, int empty = 0)
+{
+	MasToPixel(Mas, x, y, W, H, empty);
+}
+
+
+
+unsigned char MatrixProcessor::MasToPixel(unsigned char* const data, int x, int y, int w, int h, int empty = 0)
+{
+	if (x > 0 && x < w&&y>0 && x < h)
+		return data[y*w + x];
+	else return empty;
 }
