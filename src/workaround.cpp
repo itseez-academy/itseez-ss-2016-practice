2ebf0b3d9da39250a30e00f938d31ffe2cd6a492
Maxim-Doronin <doronin.maxim.unn@gmail.com>
2016-07-04 15:09:56 +0300

    Change Threashold

--- src/workaround.cpp
+++ src/workaround.cpp
@@ -7,5 +7,6 @@ using namespace std;
 void MatrixProcessor::Threshold(unsigned char* const data, const int width,
                                 const int height, const int threshold) {
 	for (int i = 0; i < width * height; i++)
-		data[i] < threshold ? data[i] = 0 : data[i] = 255;
+		if (data[i] < threshold) 
+			data[i] = 0;
 }
