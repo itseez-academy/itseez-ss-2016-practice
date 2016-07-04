54e2a3a83e897a3c14fab474d20210529ab208f6
OsechinS <speeyn@mail.ru>
2016-07-04 19:11:46 +0400

    implement average filter

--- src/workaround.cpp
+++ src/workaround.cpp
@@ -14,3 +14,13 @@ void MatrixProcessor::Threshold(unsigned char* const data, const int width,
 			data[i] = 0;
 	}
 }
+
+void MatrixProcessor::Averaging(unsigned char * const data, const int width, const int height)
+{
+	for (int i = 1; i < width - 1; ++i)
+		for (int j = 1; j < height - 1; ++j)
+			data[i*width + j] = data[(i-1)*width + j -1] + data[(i-1)*width + j] + data[(i-1)*width + j + 1] + 
+			                    data[i*width + j - 1] + data[i*width + j] + data[i*width + j + 1] +
+			                    data[(i+1)*width + j - 1] + data[(i+1)*width + j]  + data[(i+1)*width + j + 1];
+}
+
