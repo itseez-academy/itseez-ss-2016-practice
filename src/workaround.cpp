3408ff2913ce24746a147b8c9dce09fa5745bbae
ChumankinYuriy <chumankinyuriy@mail.ru>
2016-07-04 16:39:36 +0400

    written binarisation implementation

--- src/workaround.cpp
+++ src/workaround.cpp
@@ -6,5 +6,8 @@ using namespace std;
 
 void MatrixProcessor::Threshold(unsigned char* const data, const int width,
                                 const int height, const int threshold) {
-  // TODO: Add thresholding logic here.
+	// TODO: Add thresholding logic here.
+	for (int i = 0; i < width*height; i++) {
+		data[i] = data[i] > threshold ? 255 : 0;
+	}
 }
