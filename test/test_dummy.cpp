8b15c7b354e8ab99f794687270e9c1b49656aef1
mazhukinevgeniy <mazhukin.evgeniy@gmail.com>
2016-07-04 16:06:10 +0300

    add test for threshold

--- test/test_dummy.cpp
+++ test/test_dummy.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <workaround.hpp>
 
 #include "opencv2/core/core.hpp"
 
@@ -11,4 +12,25 @@ TEST(dummy, dummy_test)
     Mat mat(3, 7, CV_8UC1, Scalar::all(0));
     Mat submat = mat(Rect(0, 0, 2, 3));
     EXPECT_FALSE(submat.isContinuous());
+}
+
+TEST(threshold, threshold_test)
+{
+	int width = 3, height = 3;
+
+	unsigned char * data = new unsigned char[9];
+	MatrixProcessor mp;
+
+	for (int t = 1; t < 10; t++)
+	{
+		for (int i = 0; i < 9; i++)
+			data[i] = i + 1; //we have to reinitialize
+
+		mp.Threshold(data, width, height, t);
+
+		for (int i = 0; i < 9; i++)
+			ASSERT_TRUE((data[i] == 0 && i + 1 < t) || (data[i] != 0 && i + 1 >= t));
+	}
+
+	delete[] data;
 }
