560eb946059cbebcf79472a29530a98267d3546b
VladimirAndreevich <walkman333.94@gmail.com>
2016-07-04 16:03:23 +0300

    Test_lena-done

--- src/workaround.cpp
+++ src/workaround.cpp
@@ -5,6 +5,13 @@
 using namespace std;
 
 void MatrixProcessor::Threshold(unsigned char* const data, const int width,
-                                const int height, const int threshold) {
-  // TODO: Add thresholding logic here.
+                                const int height, const int threshold) 
+{
+	int weight = width*height;
+	for (int i = 0; i <weight; i++)
+	{
+		if (data[i] > threshold)
+			data[i] = 255;
+		else data[i] = 0;
+	}
 }
