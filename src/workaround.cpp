932135c52a4d734ea98be89261b255b6c635a54e
OlgaAndreeva <andreevaov@gmail.com>
2016-07-04 17:25:17 +0300

    Threshold function + test

--- src/workaround.cpp
+++ src/workaround.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 void MatrixProcessor::Threshold(unsigned char* const data, const int width,
                                 const int height, const int threshold) {
-  // TODO: Add thresholding logic here.
 
 	for (int i = 0; i < width; i++)
 	{
--- test/test_dummy.cpp
+++ test/test_dummy.cpp
@@ -3,20 +3,10 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/core.hpp"
 #include "opencv2/highgui.hpp"
-#include <iostream>
-
 #include "workaround.hpp"
 
+#include <iostream>
 using namespace cv;
-
-/*
-TEST(dummy, dummy_test)
-{
-    Mat mat(3, 7, CV_8UC1, Scalar::all(0));
-    Mat submat = mat(Rect(0, 0, 2, 3));
-    EXPECT_FALSE(submat.isContinuous());
-}
-*/
 
 TEST(threshold_case1, threshold_test)
 {
