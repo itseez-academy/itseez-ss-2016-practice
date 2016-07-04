d9fecbfd4090874451edaab2211eee71f2597b1d
mazhukin.evgeniy <mazhukin.evgeniy@gmail.com>
2016-07-05 00:09:18 +0300

    Implement median

--- include/workaround.hpp
+++ include/workaround.hpp
@@ -4,5 +4,8 @@ class MatrixProcessor {
  public:
   void Threshold(unsigned char* const data, const int width, const int height,
                  const int threshold);
+
   void Average(unsigned char * const data, const int width, const int height, const int range);
+
+  void Median(unsigned char * const data, const int width, const int height, const int range);
 };
--- src/workaround.cpp
+++ src/workaround.cpp
@@ -1,6 +1,7 @@
 #include "workaround.hpp"
 
 #include <cstddef>
+#include <algorithm>
 
 using namespace std;
 
@@ -47,5 +48,44 @@ void MatrixProcessor::Average(unsigned char * const data, const int width, const
 			}
 
 		delete[] newData;
+	}
+}
+
+void MatrixProcessor::Median(unsigned char * const data, const int width, const int height, const int range)
+{
+	int maskWidth = range * 2 + 1;
+	int maskSize = maskWidth * maskWidth;
+
+	int minX = range, minY = range;
+	int maxX = width - range, maxY = height - range; //it's more than max, actually
+
+	if (height >= maskWidth && width >= maskWidth)
+	{
+		int changedSubmatrixWidth = maxX - minX;
+		int changedSubmatrixHeight = maxY - minY;
+
+		unsigned char * charsToSort = new unsigned char[maskSize];
+		unsigned char * newData = new unsigned char[changedSubmatrixWidth * changedSubmatrixHeight];
+
+		for (int i = minX; i < maxX; i++)
+			for (int j = minY; j < maxY; j++)
+			{
+				for (int ii = i - range; ii <= i + range; ii++)
+					for (int jj = j - range; jj <= j + range; jj++)
+						charsToSort[ii - (i - range) + (jj - (j - range)) * maskWidth] = data[ii + jj * width];
+
+				sort(charsToSort, charsToSort + maskSize);
+
+				newData[i - minX + (j - minY) * changedSubmatrixWidth] = charsToSort[maskSize / 2];
+			}
+
+		for (int i = minX; i < maxX; i++)
+			for (int j = minY; j < maxY; j++)
+			{
+				data[i + j * width] = newData[i - minX + (j - minY) * changedSubmatrixWidth];
+			}
+
+		delete[] newData;
+		delete[] charsToSort;
 	}
 }
