f0dae6d2a7ed50a25bac11a940aed04205b9ce70
Andrey Kochuev <kochuevai@gmail.com>
2016-07-04 18:25:36 +0400

    y

--- src/workaround.cpp
+++ src/workaround.cpp
@@ -5,6 +5,16 @@
 using namespace std;
 
 void MatrixProcessor::Threshold(unsigned char* const data, const int width,
-                                const int height, const int threshold) {
+                                const int height, const int threshold) 
+{
+	int _width = width;
+	int _height = height;
+	int _threshold = threshold;
+	unsigned char* const _data = data;
+	for (int i; i < _width*_height; i++)
+	{
+		if (_data[i] < _threshold)
+			_data[i] = 0;
+	}
   // TODO: Add thresholding logic here.
 }
