e6946072f8e9d0af8dea4b22ad531ed5d464c706
mazhukinevgeniy <mazhukin.evgeniy@gmail.com>
2016-07-04 17:28:27 +0300

    implement median

--- include/workaround.hpp
+++ include/workaround.hpp
@@ -4,4 +4,5 @@ class MatrixProcessor {
  public:
   void Threshold(unsigned char* const data, const int width, const int height,
                  const int threshold);
+  void Median(unsigned char * const data, const int width, const int height, const int range);
 };
--- src/workaround.cpp
+++ src/workaround.cpp
@@ -12,3 +12,28 @@ void MatrixProcessor::Threshold(unsigned char* const data, const int width,
 		data[i] = tmp < threshold ? 0 : tmp;
 	}
 }
+
+
+void MatrixProcessor::Median(unsigned char * const data, const int width, const int height, const int range)
+{
+	int maskWidth = range * 2 + 1;
+	int maskSize = maskWidth * maskWidth;
+
+	int minX = range, minY = range;
+	int maxX = width - range, maxY = height - range;
+
+	if (height >= maskWidth && width >= maskWidth)
+	{
+		for (int i = minX; i < maxX; i++)
+			for (int j = minY; j < maxY; j++)
+			{
+
+				int sum = 0;
+				for (int ii = i - range; ii < i + range; ii++)
+					for (int jj = j - range; jj < j + range; jj++)
+						sum += data[ii + jj * width];
+
+				data[i + j * width] = sum / maskSize;
+			}
+	}
+}
