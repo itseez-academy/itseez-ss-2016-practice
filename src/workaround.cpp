a3d4d99c186799823bf12144206a1b32a2ad2158
OsechinS <speeyn@mail.ru>
2016-07-04 19:27:32 +0400

    main change filter

--- src/workaround.cpp
+++ src/workaround.cpp
@@ -19,8 +19,8 @@ void MatrixProcessor::Averaging(unsigned char * const data, const int width, con
 {
 	for (int i = 1; i < width - 1; ++i)
 		for (int j = 1; j < height - 1; ++j)
-			data[i*width + j] = data[(i-1)*width + j -1] + data[(i-1)*width + j] + data[(i-1)*width + j + 1] + 
+			data[i*width + j] = (data[(i-1)*width + j -1] + data[(i-1)*width + j] + data[(i-1)*width + j + 1] + 
 			                    data[i*width + j - 1] + data[i*width + j] + data[i*width + j + 1] +
-			                    data[(i+1)*width + j - 1] + data[(i+1)*width + j]  + data[(i+1)*width + j + 1];
+			                    data[(i+1)*width + j - 1] + data[(i+1)*width + j]  + data[(i+1)*width + j + 1])/9;
 }
 
