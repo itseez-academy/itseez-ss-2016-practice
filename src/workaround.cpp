54fab98c9ff4157c071bb545ebc567eeab5af30b
Maxim-Doronin <doronin.maxim.unn@gmail.com>
2016-07-04 16:33:26 +0300

    Add Averaging method

--- src/workaround.cpp
+++ src/workaround.cpp
@@ -10,3 +10,22 @@ void MatrixProcessor::Threshold(unsigned char* const data, const int width,
 		if (data[i] < threshold) 
 			data[i] = 0;
 }
+
+void MatrixProcessor::Averaging(unsigned char* const data, const int width,
+								const int height, const int surroundings) {
+	int maxSize = width * height;
+	for (int i = 0; i < width * height; i++) {
+		int sum = 0;
+		int count = 0;
+		for (int j = -surroundings; j < surroundings + 1; j++)
+			for (int k = -surroundings; k < surroundings + 1; k++) {
+				int idx = i + j*width + k;
+				if ((idx < 0) || (idx > maxSize))
+					continue;
+				sum += data[i + j*width + k];
+				count++;
+			}
+		data[i] = sum / count;
+
+	}
+}
