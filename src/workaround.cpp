6fbdacfa369b9dcd6ecae1f39ce12d50d5bd12f8
MChernysh <max_wood@mail.ru>
2016-07-04 16:20:06 +0300

    Threshold

--- src/workaround.cpp
+++ src/workaround.cpp
@@ -6,5 +6,8 @@ using namespace std;
 
 void MatrixProcessor::Threshold(unsigned char* const data, const int width,
                                 const int height, const int threshold) {
+	for (int i = 0; i < width * height; ++i) {
+		if (data[i] < threshold) data[i] = 0;
+	}
   // TODO: Add thresholding logic here.
 }
--- test/test_dummy.cpp
+++ test/test_dummy.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 
+#include "workaround.hpp"
+
 using namespace cv;
 
 TEST(dummy, dummy_test)
@@ -11,4 +13,12 @@ TEST(dummy, dummy_test)
     Mat mat(3, 7, CV_8UC1, Scalar::all(0));
     Mat submat = mat(Rect(0, 0, 2, 3));
     EXPECT_FALSE(submat.isContinuous());
+}
+
+TEST(threshold, threshold_test) 
+{
+	Mat mat(3, 3, CV_8UC1, 100);
+	MatrixProcessor proc;
+	proc.Threshold(mat.data, mat.cols, mat.rows, 128);
+	EXPECT_EQ(0.0, cv::norm(mat.reshape(1, 1), NORM_L1));
 }
