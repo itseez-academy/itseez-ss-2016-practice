bf63a9bc7c19813a0367fab43e7828626eb3a712
EmelinEgor <egr_95@mail.ru>
2016-07-04 19:30:43 +0400

    Solution for second task

--- src/workaround.cpp
+++ src/workaround.cpp
@@ -8,9 +8,8 @@ void MatrixProcessor::Threshold(unsigned char* const data, const int width,
                                 const int height, const int threshold) {
   // TODO: Add thresholding logic here.
 	int length = width*height;
-	for (int i = 0; i < length; i++)
+	for (int i = width+1; i < length-641; i++)
 	{
-		if (data[i] < threshold)
-			data[i] = 0;
+		data[i] = (data[i - width+1] + data[i - width] + data[i - width-1] + data[i-1] + data[i] + data[i+1] + data[i + width-1] + data[i + width] + data[i + width+1]) / 9;
 	}
 }
