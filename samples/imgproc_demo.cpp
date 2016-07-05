6716555d8df54ec771a600372e5221f8c94151de
mazhukinevgeniy <mazhukin.evgeniy@gmail.com>
2016-07-05 17:46:11 +0300

    Handle mouse events; -gray and -edges are broken for now

--- samples/imgproc_demo.cpp
+++ samples/imgproc_demo.cpp
@@ -9,6 +9,78 @@
 using namespace std;
 using namespace cv;
 
+bool gray = false;
+bool median = false;
+bool edges = false;
+bool pixel = false;
+Mat src;
+
+void processImage(const cv::Mat &src, const cv::Rect &roi)
+{
+	ImageProcessorImpl ipi;
+	Mat newMat;
+
+	if (gray)
+	{
+		newMat = ipi.CvtColor(src, roi);
+	}
+	else if (median)
+	{
+		newMat = ipi.Filter(src, roi, 5);
+	}
+	else if (edges)
+	{
+		newMat = ipi.DetectEdges(src, roi, 5, 1, 1, 3);
+	}
+	else if (pixel)
+	{
+		newMat = ipi.Pixelize(src, roi, 3);
+	}
+	else
+		return;
+
+	// Show destination image.
+	const string kDstWindowName = "Processed image";
+	namedWindow(kDstWindowName, WINDOW_NORMAL);
+	resizeWindow(kDstWindowName, src.cols, src.rows);
+	imshow(kDstWindowName, newMat);
+	waitKey();
+}
+
+
+struct MouseCallbackState {
+	bool is_selection_started;
+	bool is_selection_finished;
+	Point point_first;
+	Point point_second;
+};
+
+MouseCallbackState mcState;
+
+
+
+void CallBackFunc(int event, int x, int y, int flags, void* userdata)
+{
+	if (event == EVENT_LBUTTONDOWN)
+	{
+		mcState.is_selection_started = true;
+		mcState.point_first = Point(x, y);
+		waitKey(40000);
+	}
+	else if (event == EVENT_MOUSEMOVE)
+	{
+		//TODO: draw rectangle if selection started
+	}
+	else if (event == EVENT_LBUTTONUP)
+	{
+		mcState.is_selection_finished = true;
+		mcState.point_second = Point(x, y);
+
+		processImage(src, Rect(mcState.point_first, mcState.point_second));
+	}
+}
+
+
 const char* kAbout =
     "Practice2";
 
@@ -23,7 +95,7 @@ const char* kOptions =
 
 int main(int argc, const char** argv) {
   // Parse command line arguments.
-  CommandLineParser parser(argc, argv, kOptions);
+	CommandLineParser parser = CommandLineParser(argc, argv, kOptions);
   parser.about(kAbout);
 
   // If help option is given, print help message and exit.
@@ -32,46 +104,44 @@ int main(int argc, const char** argv) {
     return 0;
   }
 
+  mcState.is_selection_started = false;
+  mcState.is_selection_finished = false;
 
   // Read image.
-  Mat src = imread(parser.get<string>(0), CV_LOAD_IMAGE_GRAYSCALE);
+  src = imread(parser.get<string>(0), CV_LOAD_IMAGE_GRAYSCALE);
   if (src.empty()) {
 	  cout << "Failed to open image file '" + parser.get<string>(0) + "'."
 		  << endl;
 	  return 0;
   }
 
+  if (parser.get<bool>("gray"))
+  {
+	  gray = true;
+  }
+  else if (parser.get<bool>("median"))
+  {
+	  median = true;
+  }
+  else if (parser.get<bool>("edges"))
+  {
+	  edges = true;
+  }
+  else if (parser.get<bool>("pix"))
+  {
+	  pixel = true;
+  }
+
   // Show source image.
   const string kSrcWindowName = "Source image";
   const int kWaitKeyDelay = 1;
   namedWindow(kSrcWindowName, WINDOW_NORMAL);
-  resizeWindow(kSrcWindowName, 640, 480);
+  resizeWindow(kSrcWindowName, src.cols, src.rows);
   imshow(kSrcWindowName, src);
   waitKey(kWaitKeyDelay);
 
-  if (parser.get<bool>("gray"))
-  {
-
-  }
-  else if (parser.get<bool>("median"))
-  {
-
-  }
-  else if (parser.get<bool>("edges"))
-  {
-
-  }
-  else if (parser.get<bool>("pix"))
-  {
-
-  }
-
-  // Show destination image.
-  const string kDstWindowName = "Destination image";
-  namedWindow(kDstWindowName, WINDOW_NORMAL);
-  resizeWindow(kDstWindowName, 640, 480);
-  imshow(kDstWindowName, src);
-  waitKey();
+  setMouseCallback("Source image", CallBackFunc);
+  waitKey(40000);
 
 
   return 0;
