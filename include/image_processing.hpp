5b05b009f52274b2b1d9d8df90d64f5b52337eb2
Andrey Kochuev <kochuevai@gmail.com>
2016-07-05 18:14:13 +0400

    prac 2

--- include/image_processing.hpp
+++ include/image_processing.hpp
@@ -3,16 +3,53 @@
 #include <memory>
 #include <string>
 
-#include "opencv2/core/core.hpp"
-
+#include "opencv2/opencv.hpp"
+#include <stdio.h>
+using namespace std;
+using namespace cv;
 class ImageProcessor {
  public:
    virtual cv::Mat CvtColor(const cv::Mat &src, const cv::Rect &roi) = 0;
-   virtual cv::Mat Filter(const cv::Mat &src, const cv::Rect &roi, 
-     const int kSize) = 0;
-   virtual cv::Mat DetectEdges(const cv::Mat &src, const cv::Rect &roi, 
-     const int filterSize, const int lowThreshold, const int ratio, 
-     const int kernelSize) = 0;
-   virtual cv::Mat Pixelize(const cv::Mat &src, const cv::Rect &roi, 
-     const int kDivs) = 0;
+   virtual cv::Mat Filter(const cv::Mat &src, const cv::Rect &roi, const int kSize) = 0;
+  /* virtual cv::Mat DetectEdges(const cv::Mat &src, const cv::Rect &roi, const int filterSize, const int lowThreshold, const int ratio, const int kernelSize) = 0;
+   virtual cv::Mat Pixelize(const cv::Mat &src, const cv::Rect &roi, const int kDivs) = 0;*/
+};
+
+class ImageProcessorImpl : public ImageProcessor {
+public:
+	Mat CvtColor(const cv::Mat &src, const cv::Rect &roi)
+	{
+		Mat src_copy(src);
+		Mat dst_gray_roi;
+		Mat dst_roi;
+		Mat src_copy_roi (src_copy,roi);
+		cvtColor(src_copy_roi, dst_gray_roi, CV_BGR2GRAY);
+		vector<Mat> channels {dst_gray_roi, dst_gray_roi, dst_gray_roi};
+		merge(channels, dst_roi);
+		dst_roi.copyTo(src_copy_roi);
+		return src_copy;
+
+	}
+	Mat Filter(const cv::Mat &src, const cv::Rect &roi,const int size)
+	{
+		Mat src_copy(src);
+		Mat src_copy_roi(src_copy, roi);
+		medianBlur(src_copy_roi, src_copy_roi,size);
+		return src_copy;
+	}
+	/*Mat DetectEdges(Mat &src, Rect &roi,const int filter_size, const int low_threshold,
+		const int ratio, const int kernel_size)
+	{
+		Mat src_copy(src);
+		Mat src_roi(src_copy, roi);
+		Mat src_gray_roi;
+		Mat gray_blurred;
+		Mat detected_edges;
+		cvtColor(src_roi, src_gray_roi, CV_BGR2GRAY);
+		blur(src_gray_roi, gray_blurred, filter_size, Point(-1, -1),BORDER_DEFAULT);
+		Canny(gray_blurred, detected_edges)
+		return src_copy;
+	}*/
+	/*virtual cv::Mat Pixelize(const cv::Mat &src, const cv::Rect &roi,
+		const int divs);*/
 };
--- samples/template_demo.cpp
+++ samples/template_demo.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 
-#include "opencv2/core.hpp"
+#include "opencv2/opencv.hpp"
+#include "image_processing.hpp"
 
 using namespace std;
 using namespace cv;
@@ -11,23 +12,51 @@ const char* kAbout =
     "own doing-something-cool applications.";
 
 const char* kOptions =
-    "{ v video        |        | video to process         }"
+    "{ @image         |        | image                    }"
     "{ h ? help usage |        | print help message       }";
 
 
 int main(int argc, const char** argv) {
+
   // Parse command line arguments.
-  CommandLineParser parser(argc, argv, kOptions);
-  parser.about(kAbout);
+	CommandLineParser parser(argc, argv, kOptions);
+	parser.about(kAbout);
 
-  // If help option is given, print help message and exit.
-  if (parser.get<bool>("help")) {
-    parser.printMessage();
-    return 0;
-  }
+	// If help option is given, print help message and exit.
+	if (parser.has("help")) {
+		parser.printMessage();
+		return 0;
+	}
 
-  // Do something cool.
-  cout << "This is empty template sample." << endl;
+	// Read image.
+	Mat src = imread(parser.get<string>(0));
+	if (src.empty()) {
+		cout << "Failed to open image file '" + parser.get<string>(0) + "'."
+			<< endl;
+		return 0;
+	}
 
+	// Show source image.
+	const string kSrcWindowName = "Source image";
+	const int kWaitKeyDelay = 1;
+	namedWindow(kSrcWindowName, WINDOW_NORMAL);
+	resizeWindow(kSrcWindowName, 640, 480);
+	imshow(kSrcWindowName, src);
+	waitKey(kWaitKeyDelay);
+
+	ImageProcessorImpl imgproc;
+	//imgproc.CvtColor(src, Rect(5, 5, 50, 50));
+
+	// Show destination image.
+	/*const string kDstWindowName = "Destination image";
+	namedWindow(kDstWindowName, WINDOW_NORMAL);
+	resizeWindow(kDstWindowName, 640, 480);
+	imshow(kDstWindowName, imgproc.CvtColor(src, Rect(5, 5, 250, 250)));*/
+
+	const string kDstWindowName2 = "Destination image 2";
+	namedWindow(kDstWindowName2, WINDOW_NORMAL);
+	resizeWindow(kDstWindowName2, 640, 480);
+	imshow(kDstWindowName2, imgproc.Filter(src, Rect(5, 5, 250, 250),1));
+	waitKey();
   return 0;
 }
