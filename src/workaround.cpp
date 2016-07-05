8430950b825e22034f90191fcc5a3ccc4b6ffeba
Pavel Druzhkov <druzhkov.paul@gmail.com>
2016-07-05 18:09:24 +0400

    add solution

--- src/workaround.cpp
+++ src/workaround.cpp
@@ -6,5 +6,10 @@ using namespace std;
 
 void MatrixProcessor::Threshold(unsigned char* const data, const int width,
                                 const int height, const int threshold) {
-  // TODO: Add thresholding logic here.
+  for (int row = 0; row < height; row++) {
+    for (int col = 0; col < width; col++) {
+      data[row * width + col] = (data[row * width + col] < threshold) ? 0 :
+        data[row * width + col];
+    }
+  }
 }
