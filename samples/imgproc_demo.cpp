caf6d414bc3ad0fd8c2b5e0320e145cde61328d0
ChumankinYuriy <chumankinyuriy@mail.ru>
2016-07-05 18:07:50 +0300

    Parced command line options

--- samples/imgproc_demo.cpp
+++ samples/imgproc_demo.cpp
@@ -14,8 +14,12 @@ const char* kAbout =
     "This is image processing test application";
 
 const char* kOptions =
-	"{ @image         |        | image to process         }"
-	"{ h ? help usage |        | print help message       }";
+	"{ @image         | <none> | image to process            }"
+	"{ gray           |        | convert ROI to gray scale   }"
+	"{ median         |        | apply median filter for ROI }"
+	"{ edges          |        | detect edges in ROI         }"
+	"{ pix            |        | pixelize ROI                }"
+	"{ h ? help usage |        | print help message          }";
 
 int main(int argc, const char** argv) {
   // Parse command line arguments.
@@ -37,16 +41,26 @@ int main(int argc, const char** argv) {
   }
 
   ImageProcessorImpl proc;
-  Mat res = proc.CvtColor(src, Rect(0, 0, src.cols-30, src.rows-30));
-  res = proc.Filter(src, Rect(10, 20, src.cols - 50, src.rows - 50), 11);
-  int filterSize = 5;
-  int lowThreshold = 50;
-  int ratio = 3;
-  int kernelSize = 5;
-  res = proc.DetectEdges(src, Rect(10, 20, src.cols - 50, src.rows - 50),
-	  filterSize, lowThreshold, ratio, kernelSize);
-  res = proc.Pixelize(src, Rect(10, 20, src.cols - 50, src.rows - 50), 5);
-  // Show source image.
+  Mat res = src.clone();
+  if (parser.has("gray")) {
+    res = proc.CvtColor(src, Rect(0, 0, src.cols - 30, src.rows - 30));
+  }
+  else if (parser.has("median")) {
+	res = proc.Filter(src, Rect(10, 20, src.cols - 50, src.rows - 50), 11);
+  }
+  else if (parser.has("edges")) {
+	  int filterSize = 5;
+	  int lowThreshold = 50;
+	  int ratio = 3;
+	  int kernelSize = 5;
+	  res = proc.DetectEdges(src, Rect(10, 20, src.cols - 50, src.rows - 50),
+		  filterSize, lowThreshold, ratio, kernelSize);
+  }
+  else if (parser.has("pix")) {
+	  res = proc.Pixelize(src, Rect(10, 20, src.cols - 50, src.rows - 50), 5);
+  }
+ 
+  // Show images.
   const string kSrcWindowName = "Source image";
   imshow(kSrcWindowName, src);
   const string kResWindowName = "Processed image";
