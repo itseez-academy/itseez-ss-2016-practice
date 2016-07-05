912a361c0f7ba4285ab2ef91a9c5ef62f5db9180
OsechinS <speeyn@mail.ru>
2016-07-05 17:45:06 +0400

    New file for image-proc

---
+++ src/ImageProcessorImpl.cpp
@@ -0,0 +1,5 @@
+#include "ImageProcessorImpl.h"
+
+
+using namespace std;
+
---
+++ src/image_processing.cpp
@@ -0,0 +1,39 @@
+#include "..\include\image_processing.hpp"
+#include "opencv2\opencv.hpp"
+using namespace std;
+using namespace cv;
+
+cv::Mat ImageProcessorImpl::CvtColor(const cv::Mat & src, const cv::Rect & roi)
+{
+
+	Mat src_copy(src);
+	
+	Mat src_copy_roi(src_copy,roi);
+
+	Mat dst_gray_roi;
+
+	cvtColor(src_copy_roi, dst_gray_roi,CV_BGR2GRAY);
+
+	vector<Mat> channels{ dst_gray_roi, dst_gray_roi, dst_gray_roi };
+	Mat dst_roi;
+	merge(channels, dst_roi);
+
+	dst_roi.copyTo(src_copy_roi);
+
+	return src_copy;
+}
+
+cv::Mat ImageProcessorImpl::Filter(const cv::Mat & src, const cv::Rect & roi, const int size)
+{
+	return cv::Mat();
+}
+
+cv::Mat ImageProcessorImpl::DetectEdges(const cv::Mat & src, const cv::Rect & roi, const int filter_size, const int low_threshold, const int ratio, const int kernel_size)
+{
+	return cv::Mat();
+}
+
+cv::Mat ImageProcessorImpl::Pixelize(const cv::Mat & src, const cv::Rect & roi, const int divs)
+{
+	return cv::Mat();
+}
