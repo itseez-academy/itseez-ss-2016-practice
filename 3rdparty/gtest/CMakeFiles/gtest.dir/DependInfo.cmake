0c7cdeb0a07aeef0d07514a21948b1b6b0fc9a40
github-account <chibisaincrada@gmail.com>
2016-07-05 22:05:14 +0300

    hey

---
+++ 3rdparty/gtest/CMakeFiles/gtest.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/chibisaincrada/itseez-ss-2016-practice/3rdparty/gtest/gtest-all.cc" "/home/chibisaincrada/itseez-ss-2016-practice/3rdparty/gtest/CMakeFiles/gtest.dir/gtest-all.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
