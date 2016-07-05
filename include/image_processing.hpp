23b6796f8d4e1bd39afb82f4052a3089736fcfe9
mazhukinevgeniy <mazhukin.evgeniy@gmail.com>
2016-07-05 15:04:57 +0300

    Add ImageProcessorImpl declaration

--- include/image_processing.hpp
+++ include/image_processing.hpp
@@ -16,3 +16,14 @@ class ImageProcessor {
    virtual cv::Mat Pixelize(const cv::Mat &src, const cv::Rect &roi, 
      const int kDivs) = 0;
 };
+
+class ImageProcessorImpl : public ImageProcessor
+{
+	virtual cv::Mat CvtColor(const cv::Mat &src, const cv::Rect &roi);
+	virtual cv::Mat Filter(const cv::Mat &src, const cv::Rect &roi, const int size);
+	virtual cv::Mat DetectEdges(const cv::Mat &src, const cv::Rect &roi,
+		const int filter_size, const int low_threshold,
+		const int ratio, const int kernel_size);
+	virtual cv::Mat Pixelize(const cv::Mat &src, const cv::Rect &roi,
+		const int divs);
+};
