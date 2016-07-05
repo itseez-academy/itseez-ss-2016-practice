5eb76f0eea20376fdcae89f329724150c15ed0f3
Alexander Khlybov <alekshlybov23@gmail.com>
2016-07-05 23:58:41 +0300

    Added ImageProcessorImpl::Filter().

--- src/image_processing.cpp
+++ src/image_processing.cpp
@@ -22,7 +22,15 @@ cv::Mat ImageProcessorImpl::CvtColor(const cv::Mat &src, const cv::Rect &roi) {
 
 cv::Mat ImageProcessorImpl::Filter(const cv::Mat &src, const cv::Rect &roi,
 		const int kSize) {
-	return cv::Mat();
+
+	cv::Mat src_copy;
+	src.copyTo(src_copy);
+
+	cv::Mat src_copy_roi = src_copy(roi);
+
+	cv::medianBlur(src_copy_roi, src_copy_roi, kSize);
+
+	return src_copy;
 }
 
 cv::Mat ImageProcessorImpl::DetectEdges(const cv::Mat &src, const cv::Rect &roi,
