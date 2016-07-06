8d523b0353351a84e3182c16897cdf27b56467c5
Maxim-Doronin <doronin.maxim.unn@gmail.com>
2016-07-06 15:44:38 +0300

    Sample implemented

--- samples/detection_demo.cpp
+++ samples/detection_demo.cpp
@@ -2,6 +2,10 @@
 #include <string>
 
 #include "opencv2/core.hpp"
+#include "detection.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2\imgproc.hpp"
+#include <vector>
 
 using namespace std;
 using namespace cv;
@@ -11,23 +15,46 @@ const char* kAbout =
     "own doing-something-cool applications.";
 
 const char* kOptions =
-    "{ v video        |        | video to process         }"
-    "{ h ? help usage |        | print help message       }";
+"{ i image        | <none> | image to process                         }"
+"{ v video        | <none> | video to process                         }"
+"{ c camera       | <none> | camera to get video from                 }"
+"{ m model        | <none> |                                          }"
+"{ h ? help usage |        | print help message                       }";
 
 
 int main(int argc, const char** argv) {
-  // Parse command line arguments.
   CommandLineParser parser(argc, argv, kOptions);
   parser.about(kAbout);
 
-  // If help option is given, print help message and exit.
   if (parser.get<bool>("help")) {
     parser.printMessage();
     return 0;
   }
 
-  // Do something cool.
-  cout << "This is empty template sample." << endl;
+  CascadeDetector detector;
+
+  Mat src;
+  src = imread(parser.get<string>("image"), 1);
+  if (src.empty()) {
+	  cout << "Failed to open image file '" + parser.get<string>(0) + "'."
+		   << endl;
+	  return 0;
+  }
+
+  vector<Rect> objects;
+  vector<double> scores;
+  detector.Init(parser.get<string>("model"));
+  detector.Detect(src, objects, scores);
+
+  for (int i = 0; i < objects.size(); i++)
+	  rectangle(src, objects[i], Scalar(0));
+  
+  const string kSrcWindowName = "Source image";
+  const int kWaitKeyDelay = 1;
+  namedWindow(kSrcWindowName, WINDOW_NORMAL);
+  resizeWindow(kSrcWindowName, 640, 480);
+  imshow(kSrcWindowName, src);
+  waitKey();
 
   return 0;
 }
