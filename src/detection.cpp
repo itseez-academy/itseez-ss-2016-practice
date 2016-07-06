f8a4f0ee094b66b2dc7d9a99cba4affd0b66f132
mazhukinevgeniy <mazhukin.evgeniy@gmail.com>
2016-07-06 14:45:08 +0300

    Add class CascadeDetector

--- src/detection.cpp
+++ src/detection.cpp
@@ -2,12 +2,33 @@
 
 #include <iostream>
 
+#include <opencv2/objdetect.hpp>
+
 using std::string;
 using std::shared_ptr;
 using namespace cv;
 
-shared_ptr<Detector> Detector::CreateDetector(const string& name) {
-  std::cerr << "Failed to create detector with name '" << name << "'"
+shared_ptr<Detector> Detector::CreateDetector(const string& name) 
+{
+	if (name == "cascade") 
+	{
+		return std::make_shared<CascadeDetector>();
+	}
+
+	std::cerr << "Failed to create detector with name '" << name << "'"
             << std::endl;
-  return nullptr;
+	return nullptr;
+}
+
+bool CascadeDetector::Init(const std::string& model_file_path)
+{
+	this->classifier = CascadeClassifier();
+
+	return this->classifier.load(model_file_path);
+}
+
+void CascadeDetector::Detect(const cv::Mat& frame, std::vector<cv::Rect>& objects,
+	std::vector<double>& scores)
+{
+	
 }
