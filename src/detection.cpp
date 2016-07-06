1ac5ead8243cb6862634bbf00f9c325e2d43de44
OsechinS <speeyn@mail.ru>
2016-07-06 15:45:24 +0400

    nothing

--- src/detection.cpp
+++ src/detection.cpp
@@ -11,3 +11,10 @@ shared_ptr<Detector> Detector::CreateDetector(const string& name) {
             << std::endl;
   return nullptr;
 }
+
+bool CascadeDetector::Init(const std::string & model_file_path)
+{
+	bool Init;
+	CascadeClassifier::load(model_file_path);
+	return false;
+}
