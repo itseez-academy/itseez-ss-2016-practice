a67498925ac12fa64d169402a6a0d3517330acbb
OlgaAndreeva <andreevaov@gmail.com>
2016-07-06 15:32:08 +0300

    test

--- samples/template_demo.cpp
+++ samples/template_demo.cpp
@@ -2,18 +2,25 @@
 #include <string>
 
 #include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+
+
 
 using namespace std;
 using namespace cv;
+
+
 
 const char* kAbout =
     "This is an empty application that can be treated as a template for your "
     "own doing-something-cool applications.";
 
 const char* kOptions =
-    "{ v video        |        | video to process         }"
-    "{ h ? help usage |        | print help message       }";
-
+"{ i image        | <none> | image to process                         }"
+"{ v video        | <none> | video to process                         }"
+"{ c camera       | <none> | camera to get video from                 }"
+"{ m model        | <none> |                                          }"
+"{ h ? help usage |        | print help message                       }";
 
 int main(int argc, const char** argv) {
   // Parse command line arguments.
@@ -27,6 +34,20 @@ int main(int argc, const char** argv) {
   }
 
   // Do something cool.
+
+ 
+  // Read image.
+  Mat src = imread("C://Users/iss2016/Andreeva/itseez - ss - 2016 - practice/test/test_data/detection/positives/intel_logo.png", CV_LOAD_IMAGE_COLOR);
+
+  cout << "Read image" << endl;
+
+  if (src.empty()) {
+	  cout << "Failed to open image file '" + parser.get<string>(0) + "'."
+		  << endl;
+	  return 0;
+  }
+
+
   cout << "This is empty template sample." << endl;
 
   return 0;
--- src/detection.cpp
+++ src/detection.cpp
@@ -1,13 +1,25 @@
 #include "detection.hpp"
 
 #include <iostream>
+#include "opencv2/objdetect.hpp"
 
-using std::string;
-using std::shared_ptr;
+
+#include <memory>
+#include <string>
+
+using namespace std;
 using namespace cv;
 
 shared_ptr<Detector> Detector::CreateDetector(const string& name) {
   std::cerr << "Failed to create detector with name '" << name << "'"
             << std::endl;
   return nullptr;
+}
+
+bool CascadeDetector::Init(const std::string& model_file_path, cv::CascadeClassifier detector) {
+	
+	if (detector.load(model_file_path))
+		return true;
+	else
+		return false;
 }
