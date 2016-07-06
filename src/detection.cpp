5650c8f86ddba97bfb4042e350645440e91876e1
Alexander Khlybov <alekshlybov23@gmail.com>
2016-07-06 08:09:34 +0300

    #2 imp. (?)

--- src/detection.cpp
+++ src/detection.cpp
@@ -7,14 +7,20 @@ using std::shared_ptr;
 using namespace cv;
 
 shared_ptr<Detector> Detector::CreateDetector(const string& name) {
-  std::cerr << "Failed to create detector with name '" << name << "'"
-            << std::endl;
-  return nullptr;
+	
+	if (name == "cascade")
+		return std::make_shared<CascadeDetector>();
+	std::cerr	<< "Failed to create detector with name '" << name << "'"
+				<< std::endl;
+	return nullptr;
 }
 
 bool CascadeDetector::Init(const std::string & model_file_path){
-	return false;
+		return detector.load(model_file_path);;
 }
 
-void CascadeDetector::Detect(const cv::Mat & frame, std::vector<cv::Rect>& objects, std::vector<double>& scores){
+void CascadeDetector::Detect(const cv::Mat & frame, std::vector<cv::Rect>& objects, std::vector<int>& scores){
+	if (!detector.empty()) {
+		detector.detectMultiScale(frame, objects, scores);
+	}
 }
