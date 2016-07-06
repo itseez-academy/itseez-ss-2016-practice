a71ea51283aa5db9c60c94b476929119260dc7e3
arechesk <unnotigkeit@yandex.ru>
2016-07-06 18:32:45 +0400

    3 laba, check detector

--- include/detection.hpp
+++ include/detection.hpp
@@ -7,7 +7,7 @@
 #include "opencv2/objdetect.hpp"
 class Detector {
  public:
-  static std::shared_ptr<Detector> CreateDetector(const std::string& name);
+  static std::shared_ptr< Detector> CreateDetector(const std::string& name);
   virtual bool Init(const std::string& model_file_path) = 0;
   virtual void Detect(const cv::Mat& frame, std::vector<cv::Rect>& objects,
                       std::vector<double>& scores) = 0;
@@ -23,7 +23,7 @@ public:
 	{
 		if (detector.empty()) return;
 		detector.detectMultiScale(frame, objects);
-
+		
 	
 	}
 
--- samples/detection_demo.cpp
+++ samples/detection_demo.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include <string>
-
+#include"detection.hpp"
 #include "opencv2/core.hpp"
-
+#include"opencv2/imgproc.hpp"
+#include"opencv2/highgui.hpp"
+#include"opencv2/objdetect.hpp"
+#include<memory>
 using namespace std;
-using namespace cv;
+//using namespace cv;
 
 const char* kAbout =
     "This is an empty application that can be treated as a template for your "
     "own doing-something-cool applications.";
 
 const char* kOptions =
-    "{ v video        |        | video to process         }"
-    "{ h ? help usage |        | print help message       }";
-
+"{ i image        | <none> | image to process                         }"
+"{ v video        | <none> | video to process                         }"
+"{ c camera       | <none> | camera to get video from                 }"
+"{ m model        | <none> |                                          }"
+"{ h ? help usage |        | print help message                       }";
 
 int main(int argc, const char** argv) {
   // Parse command line arguments.
-  CommandLineParser parser(argc, argv, kOptions);
+  cv::CommandLineParser parser(argc, argv, kOptions);
   parser.about(kAbout);
 
   // If help option is given, print help message and exit.
@@ -27,7 +32,43 @@ int main(int argc, const char** argv) {
   }
 
   // Do something cool.
-  cout << "This is empty template sample." << endl;
+  std::shared_ptr<Detector> detector = CascadeDetector::CreateDetector("cascade");
+  (*detector).Init("unn_old_logo_cascade.xml");
+  cv::Mat frame = cv::imread("lena.png");
+  std::vector<cv::Rect> objects;
+  std::vector<double> score;
+/*
+ (*detector).Detect(frame,objects,score);
+  cout << objects.size();
+  for (auto i = objects.begin(); i != objects.end(); i++)
+  {
+	  cv::rectangle(frame, *i, cv::Scalar(0, 0, 255));
+  }
 
+ 
+  
+  cv::imshow("win", frame);
+  cv::waitKey();
+*/
+ cv::VideoCapture capture("logo.mp4");
+ cout << capture.isOpened();
+ for (;;)
+ {
+	 capture >> frame;
+	 cout << objects.size();
+	 if (frame.empty())
+	 {
+		 cout << "End of Sequence" << endl;
+		 break;
+	 }
+	 for (auto i = objects.begin(); i != objects.end(); i++)
+	 {
+		 cv::rectangle(frame, *i, cv::Scalar(0, 0, 255));
+	 }
+	 imshow("Win", frame);
+
+	 if (cv::waitKey(200) == 27)
+		 break;
+ }
   return 0;
 }
