dca69174e37d3b54cc6165017b1f45ee31527878
Alexander Khlybov <alekshlybov23@gmail.com>
2016-07-06 07:48:29 +0300

    Initial commit for practice #3.

--- include/detection.hpp
+++ include/detection.hpp
@@ -2,7 +2,8 @@
 
 #include <memory>
 #include <string>
-
+#include "opencv2/imgproc.hpp"
+#include "opencv2/opencv.hpp"
 #include "opencv2/core/core.hpp"
 
 class Detector {
@@ -11,4 +12,13 @@ class Detector {
   virtual bool Init(const std::string& model_file_path) = 0;
   virtual void Detect(const cv::Mat& frame, std::vector<cv::Rect>& objects,
                       std::vector<double>& scores) = 0;
+};
+
+class CascadeDetector : public Detector {
+protected:
+	cv::CascadeClassifier detector;
+public:
+	virtual bool Init(const std::string& model_file_path);
+	virtual void Detect(const cv::Mat& frame, std::vector<cv::Rect>& objects,
+		std::vector<double>& scores);
 };
