59b0497af0c00254d5dba76fc62c09a3f6c46d96
Школа Itseez2016 <iss2016@ccam.cmc.unn.net>
2016-07-06 18:34:17 +0300

    Detection commit

--- include/detection.hpp
+++ include/detection.hpp
@@ -4,11 +4,31 @@
 #include <string>
 
 #include "opencv2/core/core.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
 
 class Detector {
  public:
   static std::shared_ptr<Detector> CreateDetector(const std::string& name);
   virtual bool Init(const std::string& model_file_path) = 0;
   virtual void Detect(const cv::Mat& frame, std::vector<cv::Rect>& objects,
-                      std::vector<double>& scores) = 0;
+                      std::vector<int>& scores) = 0;
 };
+
+class CascadeDetector : public Detector {
+public:
+	virtual bool Init(const std::string& model_file_path);
+	virtual void Detect(const cv::Mat& frame, std::vector<cv::Rect>& objects,
+		std::vector<int>& scores);
+
+protected:
+	cv::CascadeClassifier detector;
+};
+/*
+class DetectorImpl : public Detector {
+public:
+	static std::shared_ptr<Detector> CreateDetector(const std::string& name);
+	virtual bool Init(const std::string& model_file_path);
+	virtual void Detect(const cv::Mat& frame, std::vector<cv::Rect>& objects,
+		std::vector<double>& scores);
+};
+*/
--- src/detection.cpp
+++ src/detection.cpp
@@ -6,8 +6,41 @@ using std::string;
 using std::shared_ptr;
 using namespace cv;
 
-shared_ptr<Detector> Detector::CreateDetector(const string& name) {
-  std::cerr << "Failed to create detector with name '" << name << "'"
+shared_ptr<Detector> Detector::CreateDetector(const string& name)
+{
+	if (name == "cascade") {
+		return std::make_shared<CascadeDetector>();
+	}
+	std::cerr << "Failed to create detector with name '" << name << "'"
             << std::endl;
-  return nullptr;
+	return nullptr;
+}
+
+bool CascadeDetector::Init(const string& model_file_path)
+{
+	
+	if (!detector.load(model_file_path))
+	{ 
+	//	printf("--(!)Error loading face cascade\n"); 
+		return false; 
+	}
+	return true;
+	//std::cerr << "Failed to create detector with name '" << "'"
+	//	<< std::endl;
+}
+
+void CascadeDetector::Detect(const cv::Mat& frame, std::vector<cv::Rect>& objects,
+	std::vector<int>& scores)
+{
+//	Mat frame_copy;
+//	frame.copyTo(frame_copy);
+	if (detector.empty())
+	{
+		printf("--(!)Error cascade has not loaded \n");
+		return;
+	}
+	//в multiscale надо передать доп. параметры!
+	detector.detectMultiScale(frame, objects, scores, 1.1, 3, 0, Size(30,30), Size());
+	std::cerr << "Just as planned"
+		<< std::endl;
 }
