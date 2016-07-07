3172bbb33a5d9a58ea3fccce96158644c8757bfb
mazhukinevgeniy <mazhukin.evgeniy@gmail.com>
2016-07-07 14:16:24 +0300

    Define MedianFlowTracker

--- include/tracking.hpp
+++ include/tracking.hpp
@@ -11,3 +11,10 @@ class Tracker {
   virtual bool Init(const cv::Mat &frame, const cv::Rect &roi) = 0;
   virtual cv::Rect Track(const cv::Mat &frame) = 0;
 };
+
+class MedianFlowTracker : public Tracker
+{
+public:
+	virtual bool Init(const cv::Mat &frame, const cv::Rect &roi);
+	virtual cv::Rect Track(const cv::Mat &frame);
+};
--- src/tracking.cpp
+++ src/tracking.cpp
@@ -4,10 +4,29 @@
 
 using std::string;
 using std::shared_ptr;
+using std::cerr;
+using std::endl;
 using namespace cv;
 
-shared_ptr<Tracker> Tracker::CreateTracker(const string &name) {
-  std::cerr << "Failed to create tracker with name '" << name << "'"
-            << std::endl;
-  return nullptr;
+shared_ptr<Tracker> Tracker::CreateTracker(const string &name) 
+{
+	if (name == "median_flow") 
+	{
+		return std::make_shared<MedianFlowTracker>();
+	}
+	else
+	{
+		cerr << "Failed to create tracker with name '" << name << "'"<< endl;
+		return nullptr;
+	}
+}
+
+bool MedianFlowTracker::Init(const cv::Mat &frame, const cv::Rect &roi)
+{
+	return false;
+}
+
+Rect MedianFlowTracker::Track(const cv::Mat &frame)
+{
+	return Rect();
 }
