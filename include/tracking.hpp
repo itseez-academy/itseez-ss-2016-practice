2ec094c258c32106562053faf33ac137f6b80acf
Nikita-K <n.yu.kudriavtsev@gmail.com>
2016-07-07 18:56:58 +0400

    Added tracking_demo.cpp

--- include/tracking.hpp
+++ include/tracking.hpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <string>
 
+#include "opencv2/opencv.hpp"
 #include "opencv2/core/core.hpp"
 
 class Tracker {
@@ -10,4 +11,20 @@ class Tracker {
   static std::shared_ptr<Tracker> CreateTracker(const std::string &name);
   virtual bool Init(const cv::Mat &frame, const cv::Rect &roi) = 0;
   virtual cv::Rect Track(const cv::Mat &frame) = 0;
+};
+
+class MedianFlowTracker : Tracker {
+public:
+	static std::shared_ptr<MedianFlowTracker> CreateTracker(const std::string &name)
+	{
+		if (name == "median_flow") {
+			return std::make_shared<MedianFlowTracker>();
+		}
+	};
+	virtual bool Init(const cv::Mat &frame, const cv::Rect &roi);
+	virtual cv::Rect Track(const cv::Mat &frame);
+
+protected:
+	cv::Rect position_;
+	cv::Mat frame_;
 };
