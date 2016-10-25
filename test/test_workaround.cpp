9031d8a232fd940599385c107646d770682b99fa
MichaelMurashov <mmv047@gmail.com>
2016-10-25 11:32:29 +0300

    add some tests

--- test/test_workaround.cpp
+++ test/test_workaround.cpp
@@ -15,11 +15,38 @@ TEST(MatrixProcessor, No_Throw_When_DataOK) {
     EXPECT_NO_THROW(mp.Threshold(data, width, height, threshold));
 }
 
-TEST(MatrixProcessor, Throw_When_DataFail) {
+/*TEST(MatrixProcessor, Throw_When_DataFail) {
     MatrixProcessor mp;
     int width = 4, height = 4;
     unsigned char* data;
     unsigned char threshold = 'e';
 
     EXPECT_ANY_THROW(mp.Threshold(data, width, height, threshold));
+}*/
+
+TEST(MatrixProcessor, Correct_Work) {
+    MatrixProcessor mp;
+    int width = 4, height = 4;
+    unsigned char* data;
+    unsigned char threshold = 'b';
+
+    data = new unsigned char[width * height];
+    for (int i = 0; i < width * height; i++)
+        data[i] = 'a' + i;
+
+    mp.Threshold(data, width, height, threshold);
+    EXPECT_EQ(0, data[0]);
+}
+
+TEST(MatrixProcessor, Correct_Work_With_One_Element) {
+    MatrixProcessor mp;
+    int width = 1, height = 1;
+    unsigned char* data;
+    unsigned char threshold = 'b';
+
+    data = new unsigned char[width * height];
+    data[0] = threshold + 1;
+
+    mp.Threshold(data, width, height, threshold);
+    EXPECT_EQ(threshold + 1, data[0]);
 }
