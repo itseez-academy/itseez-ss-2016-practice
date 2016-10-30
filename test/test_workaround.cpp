3d283a0ea39d329f9b548da79bc318c795ddeb02
MichaelMurashov <mmv047@gmail.com>
2016-10-30 11:01:14 +0300

    add tests for averaging

--- test/test_workaround.cpp
+++ test/test_workaround.cpp
@@ -1,69 +1,83 @@
 #include <gtest/gtest.h>
-#include "string"
 
 #include "workaround.hpp"
 
 TEST(MatrixProcessor, Threshold_No_Throw_When_DataOK) {
-    MatrixProcessor mp;
-    int width = 4, height = 4;
-    unsigned char* data;
-    unsigned char threshold = 5;
+  MatrixProcessor mp;
+  int width = 4, height = 4;
+  unsigned char* data;
+  unsigned char threshold = 5;
 
-    data = new unsigned char[width * height];
-    for (int i = 0; i < width * height; i++)
-        data[i] = i;
+  data = new unsigned char[width * height];
+  for (int i = 0; i < width * height; i++)
+    data[i] = i;
 
-    EXPECT_NO_THROW(mp.Threshold(data, width, height, threshold));
+  EXPECT_NO_THROW(mp.Threshold(data, width, height, threshold));
 }
 
 TEST(MatrixProcessor, Threshold_Correct_Work) {
-    MatrixProcessor mp;
-    int width = 4, height = 4;
-    unsigned char* data;
-    unsigned char threshold = 5;
+  MatrixProcessor mp;
+  int width = 4, height = 4;
+  unsigned char* data;
+  unsigned char threshold = 5;
 
-    data = new unsigned char[width * height];
-    for (int i = 0; i < width * height; i++)
-        data[i] = i;
+  data = new unsigned char[width * height];
+  for (int i = 0; i < width * height; i++)
+    data[i] = i;
 
-    mp.Threshold(data, width, height, threshold);
-    EXPECT_EQ(0, data[0]);
+  mp.Threshold(data, width, height, threshold);
+  EXPECT_EQ(0, data[0]);
 }
 
 TEST(MatrixProcessor, Threshold_Correct_Work_With_One_Element) {
-    MatrixProcessor mp;
-    int width = 1, height = 1;
-    unsigned char* data;
-    unsigned char threshold = 6;
+  MatrixProcessor mp;
+  int width = 1, height = 1;
+  unsigned char* data;
+  unsigned char threshold = 6;
 
-    data = new unsigned char[width * height];
-    data[0] = threshold + 1;
+  data = new unsigned char[width * height];
+  data[0] = threshold + 1;
 
-    mp.Threshold(data, width, height, threshold);
-    EXPECT_EQ(threshold + 1, data[0]);
+  mp.Threshold(data, width, height, threshold);
+  EXPECT_EQ(threshold + 1, data[0]);
 }
 
 TEST(MatrixProcessor, Averaging_Correct_Work) {
-    MatrixProcessor mp;
-    const int width = 3, height = 3;
-    unsigned char* data;
-    int surroundings = 1;
+  MatrixProcessor mp;
+  const int width = 3, height = 3;
+  unsigned char* data;
+  int surroundings = 1;
 
-    data = new unsigned char[width * height];
-    for (int i = 0; i < width * height; i++)
-        data[i] = i;
+  data = new unsigned char[width * height];
+  for (int i = 0; i < width * height; i++)
+    data[i] = i;
 
-    unsigned char expected[width * height] = { '2','2','3','3','4','4','5','5','6' };
-    mp.Averaging(data, width, height,surroundings);
-    bool flag = true;
+  unsigned char expected[width * height] = { 2,2,3,3,4,4,5,5,6 };
+  mp.Averaging(data, width, height,surroundings);
+  bool flag = true;
 
-    std::string str;
-    for (int i = 0; i < width * height; i++)
-        str[i] = data[i];
+  for (int i = 0; i < width * height; i++)
+    if (data[i] != expected[i])
+      flag = false;
 
-    for (int i = 0; i < width * height; i++)
-        if (data[i] != expected[i])
-            flag = false;
+  EXPECT_TRUE(flag);
+}
 
-    EXPECT_TRUE(flag);
+TEST(MatrixProcessor, Averaging_Correct_Work_With_Big_Surroudings) {
+  MatrixProcessor mp;
+  const int width = 5, height = 5;
+  unsigned char* data;
+  int surroundings = 4;
+  int expected = 0;
+
+  data = new unsigned char[width * height];
+  for (int i = 0; i < width * height; i++) {
+    data[i] = i;
+    expected += i;
+  }
+
+  expected /= width * height;
+  mp.Averaging(data, width, height, surroundings);
+
+  EXPECT_EQ(expected, data[0]);
 }
