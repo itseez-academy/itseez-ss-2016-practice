32f536c06a8ebfdafec5ea48e42222c64b53b695
JuliaKamelina <juliak.d@mail.ru>
2017-10-21 21:08:49 +0300

    add video_proc

--- src/image_processing.cpp
+++ src/image_processing.cpp
@@ -10,19 +10,14 @@ using cv::Rect;
 using std::vector;
 
 Mat ImageProcessorImpl::CvtColor(const Mat &src, const Rect &roi) {
-  Mat src_copy;
+  Mat src_copy, dst_roi;
   src.copyTo(src_copy);
 
   Mat src_roi = src_copy(roi), dst_gray_roi;
   cvtColor(src_roi, dst_gray_roi, CV_BGR2GRAY);
 
-  vector<Mat> channels;
-  channels.push_back(dst_gray_roi);
-  channels.push_back(dst_gray_roi);
-  channels.push_back(dst_gray_roi);
-  Mat dst_roi;
+  vector<Mat> channels(3, dst_gray_roi);
   merge(channels, dst_roi);
-
   dst_roi.copyTo(src_roi);
 
   return src_copy;
@@ -46,7 +41,6 @@ Mat ImageProcessorImpl::DetectEdges(const Mat &src, const Rect &roi,
   cvtColor(src_roi, src_gray_roi, CV_BGR2GRAY);
 
   Mat gray_blurred;
-
   blur(src_gray_roi, gray_blurred, cv::Size(size, size));
 
   Mat detected_edges;
@@ -69,9 +63,9 @@ Mat ImageProcessorImpl::Pixelize(const Mat &src, const Rect &roi,
   Mat src_copy_roi = src_copy(roi);
 
   int block_size_x = roi.width / divs, block_size_y = roi.height / divs;
-  for (int x = 0; x < roi.width - block_size_x; x += block_size_x) {
-    for (int y = 0; y < roi.height - block_size_y; y += block_size_y) {
-      Mat src_roi_block = src_copy_roi(Rect(x, y, block_size_x, block_size_y));
+  for (int i = 0; i < roi.width - block_size_x; i += block_size_x) {
+    for (int j = 0; j < roi.height - block_size_y; j += block_size_y) {
+      Mat src_roi_block = src_copy_roi(Rect(i, j, block_size_x, block_size_y));
       blur(src_roi_block, src_roi_block, cv::Size(block_size_x, block_size_y));
     }
   }
