5eaeaa23f546d2b477b2219581223e31a9575d63
JuliaKamelina <juliak.d@mail.ru>
2017-10-22 22:49:10 +0300

    doesn't work for images

---
+++ samples/detection_demo.cpp
@@ -0,0 +1,103 @@
+#include "detection.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+using std::string;
+using cv::imread;
+using cv::imshow;
+using cv::namedWindow;
+
+const char* kOptions =
+"{ i image        | <none> | image to process                         }"
+"{ v video        | <none> | video to process                         }"
+"{ c camera       | <none> | camera to get video from                 }"
+"{ m model        | <none> | path to detector file                    }"
+"{ h ? help usage |        | print help message                       }";
+
+int main(int argc, const char** argv) {
+  // Parse command line arguments.
+  cv::CommandLineParser parser(argc, argv, kOptions);
+
+  // If help option is given, print help message and exit.
+  if (parser.get<bool>("help")) {
+    parser.printMessage();
+    return 0;
+  }
+
+  CascadeDetector det;
+
+  string det_path = parser.get<string>("model");
+  det.Init(det_path);
+
+  cv::Mat img;
+  std::vector<cv::Rect> obj;
+  std::vector<double> score;
+
+  string windName = "Source";
+  namedWindow(windName);
+
+  if (parser.get<bool>("image")) {  //  IMAGE
+    img = imread(parser.get<string>(0), cv::IMREAD_COLOR);
+    if (img.empty()) {
+      std::cout << "Failed to open image file" << std::endl;
+      return 0;
+    }
+
+    imshow(windName, img);
+    cv::waitKey(10);
+
+    det.Detect(img, obj, score);
+
+    for (auto it = obj.begin(); it != obj.end(); ++it) {
+      cv::rectangle(img, *it, cv::Scalar(0, 0, 255), 2);
+    }
+
+    imshow(windName, img);
+    cv::waitKey();
+  } else {  //  VIDEO
+    cv::VideoCapture cap;
+
+    if (parser.get<bool>("video")) {
+      string v_path = parser.get<string>("video");
+      cap.open(v_path);
+
+      if (!cap.isOpened()) {
+        std::cout << "Failed to get video." << std::endl;
+        return 0;
+      }
+    } else {
+      if (parser.get<bool>("camera")) {
+        int id = parser.get<int>("camera");
+        cap.open(id);
+
+        if (!cap.isOpened()) {
+          std::cout << "Failed to capture video from camera" << std::endl;
+          return 0;
+        }
+      }
+    }
+    cap >> img;
+
+    for (;;) {
+      det.Detect(img, obj, score);
+
+      for (auto it = obj.begin(); it != obj.end(); ++it) {
+        cv::rectangle(img, *it, cv::Scalar(0, 0, 255), 2);
+      }
+
+      imshow(windName, img);
+
+      char c = cv::waitKey(20);
+      if (c == 27 || img.empty()) break;
+
+      cap >> img;
+    }
+  }
+
+  return 0;
+}
--- src/detection.cpp
+++ src/detection.cpp
@@ -7,7 +7,37 @@ using std::shared_ptr;
 using namespace cv;
 
 shared_ptr<Detector> Detector::CreateDetector(const string& name) {
-  std::cerr << "Failed to create detector with name '" << name << "'"
-            << std::endl;
-  return nullptr;
+  if (name == "cascade") {
+    return std::make_shared<CascadeDetector>();
+  } else {
+    std::cerr << "Failed to create detector with name '" << name << "'"
+              << std::endl;
+    return nullptr;
+  }
+}
+
+bool CascadeDetector::Init(const std::string& model_file_path) {
+  if (!detector.load(model_file_path)) {
+    std::cout << "Couldn't load cascade detector" << std::endl;
+    return false;
+  } else {
+    return true;
+  }
+}
+
+void CascadeDetector::Detect(const cv::Mat& frame, std::vector<cv::Rect>& objects,
+  std::vector<double>& scores) {
+  if (detector.empty()) {
+    std::cout << "Detector is not loaded" << std::endl;
+    return;
+  }
+
+  std::vector<int> t;
+  detector.detectMultiScale(frame, objects, t);
+
+  scores.resize(t.size());
+  int i = 0;
+  for (auto it = t.begin(); it != t.end(); ++it) {
+    scores[i++] = *it;
+  }
 }
