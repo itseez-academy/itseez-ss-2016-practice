97567dcf6286b3efc0337180a6c9009aabbdca95
ivanvikhrev <laind4471@gmail.com>
2018-09-22 23:43:14 +0300

    Pixelize

--- samples/imgproc_demo.cpp
+++ samples/imgproc_demo.cpp
@@ -22,36 +22,45 @@ const char* kOptions =
 "{ pix            |        | pixelize ROI                }"
 "{ h ? help usage |        | print help message          }";
 
-struct MouseCallbackState{
+struct MouseCallbackState
+{
    bool is_selection_started;
    bool is_selection_finished;
    Point point_first;
    Point point_second;
 };
-MouseCallbackState mouse;
+//MouseCallbackState mouse;
 void OnMouse(int event, int x, int y, int flags, void* param)
 {	
+	MouseCallbackState* mouse = (MouseCallbackState*)param;
+
 	switch (event)
 	{
 	case EVENT_LBUTTONDOWN:
 
-		mouse.is_selection_started = true;
-		mouse.is_selection_finished = false;
-		mouse.point_first.x = x;
-		mouse.point_first.y = y;
+
+		mouse->is_selection_started = true;
+		mouse->is_selection_finished = false;
+		mouse->point_first.x = x;
+		mouse->point_first.y = y;
 		break;
 	case EVENT_LBUTTONUP:
-		mouse.is_selection_started = false;
-		mouse.is_selection_finished = true;
-		mouse.point_second.x = x;
-		mouse.point_second.y = y;
-		break;
-	case EVENT_MOUSEMOVE:
-		if (mouse.is_selection_finished = false)
+		mouse->is_selection_started = false;
+		mouse->is_selection_finished = true;
+		if (mouse->point_first.x != x && mouse->point_first.y != y)
 		{
-			mouse.point_second.x = x;
-			mouse.point_second.y = y;
+			mouse->point_second.x = x;
+			mouse->point_second.y = y;
 		}
+		//rectangle(,Rect(mouse->point_first,mouse->point_second));
+		break;
+	    case EVENT_MOUSEMOVE:
+		if (mouse->is_selection_finished == false)
+		{
+			mouse->point_second.x = x;
+			mouse->point_second.y = y;
+		}
+		break;
 	}
 	
 }
@@ -60,33 +69,41 @@ int main(int argc, const char** argv) {
   CommandLineParser parser(argc, argv, kOptions);
   parser.about(kAbout);
   Mat img,src,dst;
-  string imageName("C:/MyProjects/itseez-ss-2016-practice/test/test_data/imgproc/lena.png");
+  string imageName("C:/MyProjects/itseez-ss-2016-practice/test/test_data/imgproc/lena.png"); //ralative path?
+  
   if (argc > 1)
 	imageName = argv[1];
+  
   img = imread(imageName.c_str(), 1);
+  
   if (img.empty())                      
 	{
 		cout << "Could not open or find the image" << std::endl;
 		return -1;
 	}
-  cvNamedWindow("Original", CV_WINDOW_AUTOSIZE);
-  cvSetMouseCallback("Original", OnMouse, &img);
-  mouse.point_first.x = 10;
-  mouse.point_first.y = 10;
-  mouse.point_second.x = 400;
-  mouse.point_second.y = 400;
-  //IplImage tmp = img;
+  namedWindow("Original", CV_WINDOW_AUTOSIZE);
+  MouseCallbackState p;
+  setMouseCallback("Original", OnMouse, &p);
+  //cvSetMouseCallback("Original", OnMouse, &img); // cv and not cv functions
+  p.point_first.x = 10;
+  p.point_first.y = 10;
+  p.point_second.x = 400;
+  p.point_second.y = 400;
+
   while (1) 
   {
-	  char c = cvWaitKey(33);
+	  char c = waitKey(33);//?
 	  imshow("Original", img);
-	  Rect roi(mouse.point_first, mouse.point_second);
-	  rectangle(img, roi, 1);
-	  ImageProcessorImpl obj;
-
+	  if (p.point_first.x != 0 && p.point_second.x != 0 && p.point_first.y != 0 && p.point_second.y != 0)
+	  {
+	      Rect roi(p.point_first, p.point_second);
+		  ImageProcessorImpl obj;
+			//dst = obj.DetectEdges(img, roi, 1, 1, 1, 1);
+		  dst = obj.Pixelize(img, roi, 10);
+		  imshow("Result", dst);
+	  }
 	  //dst = obj.CvtColor(img, roi);
-	  dst = obj.Filter(img, roi, 11);
-	  imshow("Result", dst);
+	 // dst = obj.Filter(img, roi, 11); // size must be odd
 	  if (c == 27) 
 	  { 
 		  break;
--- src/image_processing.cpp
+++ src/image_processing.cpp
@@ -8,27 +8,26 @@ Mat ImageProcessorImpl::CvtColor(const Mat &src, const Rect &roi)
 	Mat src_copy = src;
 	Mat src_copy_roi = src_copy(roi);
 	Mat dst_gray_roi, dst_gray;
-	/*Mat dst_roi;
+	Mat dst_roi = src_copy(roi);
 	std::vector<Mat> channels;
-	cvtColor(src_copy_roi, dst_gray_roi, COLOR_BGR2GRAY);
+	cvtColor(src_copy_roi, dst_gray_roi, COLOR_BGR2GRAY); // RGB или BGR или ... ?
 	channels.push_back(dst_gray_roi);
 	channels.push_back(dst_gray_roi);
 	channels.push_back(dst_gray_roi);
 	merge(channels, dst_roi);
-	src_copy_roi = dst_roi;*/
-	cvtColor(src_copy, dst_gray, COLOR_BGR2GRAY);
-	return dst_gray;
+	src_copy_roi = dst_roi;
+	return src_copy;
 }
 
 cv::Mat ImageProcessorImpl::Filter(const Mat &src, const Rect &roi, const int size)
 {	
-	Mat src_copy(src);
+	Mat src_copy(src); //??
 	Mat src_copy_roi = src_copy(roi);
-	medianBlur(src_copy_roi,src_copy,size);
+	medianBlur(src_copy_roi,src_copy_roi,size);//
 	return src_copy;
 	//return cv::Mat(); ?
 }
-
+// для чего нужны аргументы? filterSize, ratio, kernelSize;
 Mat ImageProcessorImpl::DetectEdges(const Mat &src, const Rect &roi,
 	const int filterSize, const int lowThreshold, const int ratio,
 	const int kernelSize)
@@ -37,19 +36,43 @@ Mat ImageProcessorImpl::DetectEdges(const Mat &src, const Rect &roi,
 	Mat src_copy_roi = src_copy(roi);
 	Mat src_gray_roi;
 	cvtColor(src_copy_roi, src_gray_roi, COLOR_BGR2GRAY);
-	Mat gray_blurred;
+	Mat gray_blurred; 
 	Mat detected_edges;
-	//blur(src_gray_roi, gray_blurred,);
-	Canny(gray_blurred, detected_edges, lowThreshold, ratio);
-	Mat dst(src);
-	Mat dst_roi = dst(roi);
-	Scalar::all(0);
-	//dst_roi = src_roi;
-
-	return Mat();
+	Size a;
+	a.width = 1;
+	a.height = 1;
+	blur(src_gray_roi, gray_blurred,a); // ? зачем фильтровать?
+	Canny(gray_blurred, detected_edges, 50, 200);
+	Mat dst;
+	src_copy.copyTo(dst);
+	Mat dst_roi = dst(roi); // когда копируешь или присваиваешь они ссылаются все на один объект?
+	dst_roi = Scalar::all(0);
+	//dst_roi = src_copy_roi(detected_edges);
+	//cvCopy(src_copy_roi, dst_roi, detected_edges);
+	src_copy_roi.copyTo(dst_roi, detected_edges);
+	return dst; // зачем обнулять? зачем копировать? почему нельзя вернуть сраpу detected_edges
 }
+
 
 cv::Mat ImageProcessorImpl::Pixelize(const cv::Mat & src, const cv::Rect & roi, const int divs)
 {
-	return cv::Mat();
+	Mat src_copy;
+	src.copyTo(src_copy);
+	Mat src_copy_roi = src_copy(roi);
+	int block_size_x = roi.width / divs;
+	int block_size_y = roi.height / divs;
+	Size kernSize(block_size_x, block_size_y);
+	Mat src_roi_block;
+	for (int y = roi.y; y < roi.y+roi.height; y++)
+		for (int x = roi.x; x < roi.x + roi.width; x++)
+		{	
+			Rect a(x, y, 0, 0);
+			if (roi.contains(Point(x + 1, y + 1)) && roi.contains(Point(x , y + 1)) && roi.contains(Point(x + 1, y)) && roi.contains(Point(x , y)))
+			{
+				src_roi_block = src_copy_roi(a);
+				blur(src_roi_block, src_copy_roi, kernSize);
+			}
+		}
+
+	return src_copy;
 }
