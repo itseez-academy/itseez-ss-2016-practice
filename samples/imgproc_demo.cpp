464b7f80279cf6bcf1b2acac989b51d567de940d
ivanvikhrev <laind4471@gmail.com>
2018-10-01 21:37:49 +0300

    some fixes

--- samples/imgproc_demo.cpp
+++ samples/imgproc_demo.cpp
@@ -10,10 +10,6 @@ const char* kAbout =
     "This is an empty application that can be treated as a template for your "
     "own doing-something-cool applications.";
 
-//const char* kOptions =
-//    "{ v video        |        | video to process         }"
-//    "{ h ? help usage |        | print help message       }";
-
 const char* kOptions =
 "{ @image         | <none> | image to process            }"
 "{ gray           |        | convert ROI to gray scale   }"
@@ -44,7 +40,9 @@ void OnMouse(int event, int x, int y, int flags, void* param)
 		mouse->point_first.x = x;
 		mouse->point_first.y = y;
 		break;
+
 	case EVENT_LBUTTONUP:
+		
 		mouse->is_selection_started = false;
 		mouse->is_selection_finished = true;
 		if (mouse->point_first.x != x && mouse->point_first.y != y)
@@ -52,9 +50,10 @@ void OnMouse(int event, int x, int y, int flags, void* param)
 			mouse->point_second.x = x;
 			mouse->point_second.y = y;
 		}
-		//rectangle(,Rect(mouse->point_first,mouse->point_second));
 		break;
-	    case EVENT_MOUSEMOVE:
+
+	case EVENT_MOUSEMOVE:
+		
 		if (mouse->is_selection_finished == false)
 		{
 			mouse->point_second.x = x;
@@ -64,40 +63,46 @@ void OnMouse(int event, int x, int y, int flags, void* param)
 	}
 	
 }
-int main(int argc, const char** argv) {
+
+int main(int argc, const char** argv) 
+{
   // Parse command line arguments.
   CommandLineParser parser(argc, argv, kOptions);
-  
   parser.about(kAbout);
+  
   // If help option is given, print help message and exit.
-if (parser.get<bool>("help"))
+  if (parser.get<bool>("help"))
 	{
 	  parser.printMessage();
 	  return 0;
 	}
+
   Mat img,src,dst;
-  string imageName(parser.get<string>(0)); 
+  MouseCallbackState p;
+  ImageProcessorImpl obj;
+  Rect roi; 
+  char c;
+
+  string imageName(parser.get<string>(0));
   img = imread(imageName.c_str(), 1);
-  
   if (img.empty())                      
 	{
 		cout << "Could not open or find the image" << std::endl;
 		return -1;
 	}
 
-  namedWindow("Original", CV_WINDOW_NORMAL);
-  MouseCallbackState p;
-  ImageProcessorImpl obj;
+  imshow("Original", img);
   setMouseCallback("Original", OnMouse, &p);
-  while (1) 
-  {
-	  char c = waitKey(100);//?
-	
-	  imshow("Original", img);
-	  Rect imgSize(Point(), img.size());
-	  Rect roi(p.point_first, p.point_second);
-	  if (p.point_first.x != 0 && p.point_second.x != 0 && p.point_first.y != 0 
-		  && p.point_second.y != 0 && imgSize.contains(p.point_first) && imgSize.contains(p.point_second) && !roi.empty())
+  while (1)
+  { 
+	  while (1)
+	  {   
+		  c = waitKey(100);
+		  roi = Rect(p.point_first, p.point_second);
+		  if ((p.point_first.x < p.point_second.x) && (p.point_first.y < p.point_second.y) && !roi.empty()) break;
+	  }
+
+	 if (!roi.empty() && (( roi & Rect(0,0,img.cols,img.rows)) == roi))
 	  {
 		  if (parser.has("gray"))
 		  {
@@ -109,32 +114,28 @@ if (parser.get<bool>("help"))
 				  int _size = 11;// size must be odd
 				  dst = obj.Filter(img, roi, 11);
 			  }
-			  else 
+			  else
 				  if (parser.has("edges"))
-				  {	
+				  {
 					  int _filter_size = 1,
 						  _low_threshold = 50,
 						  _ratio = 4,
-					      _ksize = 1;
-					  dst = obj.DetectEdges(img, roi, _filter_size,_low_threshold , _ratio, _ksize);
+						  _ksize = 1;
+					  dst = obj.DetectEdges(img, roi, _filter_size, _low_threshold, _ratio, _ksize);
 				  }
 				  else
 					  if (parser.has("pix"))
-					  {		
-						  int _divs = 53;
-						  roi = Rect(Point(0, 0), Point(500, 500));
-						  dst = obj.Pixelize(img, roi, _divs); // crashes periodically
+					  {
+						  int _divs = 11;
+
+						  dst = obj.Pixelize(img, roi, _divs);
 					  }
-		  imshow("Result", dst);
-	  }
-	  if (c == 27) 
-	  { 
-		  break;
-	  }
+	  imshow("Result", dst);
+      }  
+	  if (c == 27) break;
   }
 
-
-  waitKey(0);
+ 
   return 0;
 }
 
--- src/image_processing.cpp
+++ src/image_processing.cpp
@@ -10,14 +10,14 @@ Mat ImageProcessorImpl::CvtColor(const Mat &src, const Rect &roi)
 	Mat dst_roi = src_copy_roi.clone(), dst_gray_roi;
 	std::vector<Mat> channels;
 
-	cvtColor(src_copy_roi, dst_gray_roi, COLOR_BGR2GRAY); // RGB или BGR или ... ?
+	cvtColor(src_copy_roi, dst_gray_roi, COLOR_BGR2GRAY); 
 	
 	channels.push_back(dst_gray_roi);
 	channels.push_back(dst_gray_roi);
 	channels.push_back(dst_gray_roi);
 	
 	merge(channels, dst_roi);
-	dst_roi.copyTo(src_copy_roi); // difference between copyTo and clone, src_copy_roi = dst_roi.clone() doesnt work
+	dst_roi.copyTo(src_copy_roi); 
 	return src_copy;
 }
 
@@ -31,43 +31,40 @@ cv::Mat ImageProcessorImpl::Filter(const Mat &src, const Rect &roi, const int si
 	return src_copy;
 	
 }
-// для чего нужны аргументы? filterSize, ratio, kernelSize;
+
 Mat ImageProcessorImpl::DetectEdges(const Mat &src, const Rect &roi,
 	const int filterSize, const int lowThreshold, const int ratio,
 	const int kernelSize)
 {
 	Mat src_copy = src.clone();
 	Mat src_copy_roi = src_copy(roi);
-	Mat src_gray_roi , gray_blurred, detected_edges, dst, dst_roi;
-	Size a(kernelSize,kernelSize);
-	
+	Mat src_gray_roi, gray_blurred, detected_edges, dst, dst_roi;
+	Size a(kernelSize, kernelSize);
+
 	cvtColor(src_copy_roi, src_gray_roi, COLOR_BGR2GRAY);
-	blur(src_gray_roi, gray_blurred,a); // ? зачем фильтровать?
+	blur(src_gray_roi, gray_blurred, a);
 	Canny(gray_blurred, detected_edges, lowThreshold, lowThreshold*ratio);
-	
+
 	dst = src.clone();
 	dst_roi = dst(roi);
 	dst_roi = Scalar::all(0);
 	src_copy_roi.copyTo(dst_roi, detected_edges);
-	return dst; // зачем обнулять? зачем копировать? почему нельзя вернуть сраpу detected_edges
+	return dst;
 }
 
-//как нумеруются элементы в подматрице?
 cv::Mat ImageProcessorImpl::Pixelize(const cv::Mat & src, const cv::Rect & roi, const int divs)
 {
-	Mat src_copy = src.clone(); // отличия clone от copyTo
+	Mat src_copy = src.clone(); 
 	Mat src_copy_roi = src_copy(roi);
 	int block_size_x = (int)(roi.width / divs), block_size_y = (int)(roi.height / divs);
 	Size kernSize(block_size_x, block_size_y);
 
-	for(int x = 0; x < src_copy_roi.cols - block_size_x; x += block_size_x)  // 0 или roi.x?
-		for (int y = 0; y < src_copy_roi.rows - block_size_y ; y += block_size_y) // 0 или roi.y
+	for(int x = 0; x < src_copy_roi.cols - block_size_x; x += block_size_x)  
+		for (int y = 0; y < src_copy_roi.rows - block_size_y ; y += block_size_y) 
 		{	
-			//if (roi.contains(Point(x + 1, y + 1)) && roi.contains(Point(x , y + 1)) && roi.contains(Point(x + 1, y)) && roi.contains(Point(x , y)))
-				Mat src_roi_block = src_copy_roi(Rect(x,y,block_size_x,block_size_y));//
+			Mat src_roi_block = src_copy_roi(Rect(x,y,block_size_x,block_size_y));
 				blur(src_roi_block, src_roi_block, kernSize);
-			
-		}
+	    }
 
 	return src_copy;
 }
